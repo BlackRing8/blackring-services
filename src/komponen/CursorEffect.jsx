705683e1e8d3f5c2e00babd9d8cbb78a6b8c44af
BlackRing8 <gprakoso123@gmail.com>
2024-02-20 17:24:20 +0700

    update bug cursor effect

--- src/komponen/CursorEffect.jsx
+++ src/komponen/CursorEffect.jsx
@@ -1,53 +1,81 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./CursorStyle.css";
+import { motion } from "framer-motion";
 
 const CursorEffect = () => {
-  const pergerakan = { x: 0, y: 0 };
-  const circles = document.querySelectorAll(".circle");
-
-  circles.forEach(function (circle) {
-    circle.x = 0;
-    circle.y = 0;
+  const [pergerakan, setPergerakan] = useState({
+    x: 0,
+    y: 0,
   });
 
-  window.addEventListener("mousemove", function (e) {
-    pergerakan.x = e.clientX;
-    pergerakan.y = e.clientY;
-  });
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPergerakan({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
 
-  function animateCircle() {
-    let x = pergerakan.x;
-    let y = pergerakan.y;
+    window.addEventListener("mousemove", mouseMove);
 
-    circles.forEach(function (circle, index) {
-      circle.style.left = x - 12 + "px";
-      circle.style.top = y - 12 + "px";
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
 
-      circle.style.scale = (circles.length - index) / circles.length;
+  // const circles = document.querySelectorAll(".circle");
 
-      circle.x = x;
-      circle.y = y;
+  // circles.forEach(function (circle) {
+  //   circle.x = 0;
+  //   circle.y = 0;
+  // });
 
-      const nextCircle = circles[index + 1] || circles[0];
-      x += (nextCircle.x - x) * 0.5;
-      y += (nextCircle.y - y) * 0.5;
-    });
-    requestAnimationFrame(animateCircle);
-  }
+  // window.addEventListener("mousemove", function (e) {
+  //   pergerakan.x = e.clientX;
+  //   pergerakan.y = e.clientY;
+  // });
 
-  animateCircle();
+  // function animateCircle() {
+  //   let x = pergerakan.x;
+  //   let y = pergerakan.y;
+
+  //   circles.forEach(function (circle, index) {
+  //     circle.style.left = x - 12 + "px";
+  //     circle.style.top = y - 12 + "px";
+
+  //     circle.style.scale = (circles.length - index) / circles.length;
+
+  //     circle.x = x;
+  //     circle.y = y;
+
+  //     const nextCircle = circles[index + 1] || circles[0];
+  //     x += (nextCircle.x - x) * 0.5;
+  //     y += (nextCircle.y - y) * 0.5;
+  //   });
+  //   requestAnimationFrame(animateCircle);
+  // }
+
+  const variants = {
+    default: {
+      x: pergerakan.x,
+      y: pergerakan.y,
+    },
+  };
+
+  // animateCircle();
   return (
     <>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
+      <motion.div className="circle" variants={variants} animate="default" />
     </>
   );
 };
