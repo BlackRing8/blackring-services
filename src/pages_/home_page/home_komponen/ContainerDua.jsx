239e645ce661c16a8f4f8aea07a754b0040b6b22
BlackRing8 <gprakoso123@gmail.com>
2024-07-11 22:18:11 +0700

    ubah color

--- src/pages_/home_page/home_komponen/ContainerDua.jsx
+++ src/pages_/home_page/home_komponen/ContainerDua.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const ContainerDua = () => {
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.5 } }} className="flex w-full h-auto rounded-xl bg-yellow-500  mb-[15px] ">
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.5 } }} className="flex w-full h-auto rounded-xl bg-[#F5F7F8]  mb-[15px] ">
       <div className="flex flex-wrap w-full h-[190px] rounded-xl"></div>
     </motion.div>
   );
--- src/pages_/home_page/home_komponen/ContainerTiga.jsx
+++ src/pages_/home_page/home_komponen/ContainerTiga.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const ContainerTiga = () => {
   return (
     <>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.7 } }} className="flex w-1/2 h-full rounded-xl bg-red-600 "></motion.div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.7 } }} className="flex w-1/2 h-full rounded-xl bg-[#F5F7F8] "></motion.div>
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.9 } }} className="grid flex-wrap w-1/2 h-full rounded-xl bg-[#F5F7F8] ">
         {/* <div className=" bg-[#F5F7F8] w-full h-full col rounded-xl c3 flex-col">
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 1.2 } }} className="fi bg-[#F5F7F8] flex sm:w-40 h-8 w-28 sm:h-10 border-2 border-black justify-center mt-5 ml-4 rounded-md items-center">
